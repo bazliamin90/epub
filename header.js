3eec4aa481cc0bdb7e847f14d9f562b16c03944f
bazliamin90 <bez.aminuddin@gmail.com>
2026-04-15 23:12:31 +0800

    Update header.js

--- header.js
+++ header.js
@@ -23,7 +23,7 @@ headerTemplate3.innerHTML = `
     border: 1px solid #ccc;
     background-color: #fff;
     border-radius: 8px;
-    padding: 2px 4px;
+    padding: 2px 6px;
     cursor: pointer;
     display: block;
     object-fit: contain;
